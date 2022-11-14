0d8948722c09ef0d5523eb835450894afa811048
jesnagbg <j.m.pettersson@hotmail.se>
2022-11-14 21:30:25 +0100

    first page got extra question

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 const itemList = ["Broom", "Potatoes"];
 const nameInput = document.getElementById("name-input");
 const nameButton = document.getElementById("start-button");
+//const spareQuestion = document.getElementById("extra-question");
 let playerName = "Farmer";
 
-window.addEventListener('DOMContentLoaded', displayItems);
+window.addEventListener("DOMContentLoaded", displayItems);
 
 nameButton.addEventListener("click", savePlayerName);
 
+
 function savePlayerName() {
-    const playerName = document.getElementById("name-input").value;
-    console.log(playerName)
+    playerName = document.getElementById("name-input").value;
+    console.log(playerName);
+    
+    oneMoreQ()
 }
 
+function oneMoreQ() {
+    const extraQuestion = document.getElementById("extra-question");
+    extraQuestion.textContent = 'Oh, right. Sorry, ' + playerName + '. Just one more thing! Chose your weapon';
+}
 
 function displayItems() {
     const div = document.getElementById("item-container");
-    const ul = document.createElement ('ul');
+    const ul = document.createElement("ul");
     ul.id = "item-list";
 
     for (const item of itemList) {
-        const li = document.createElement('li');
+        const li = document.createElement("li");
         li.textContent = item;
         ul.append(li);
     }
 
     div.innerHTML = "";
-    div.append(ul);    
+    div.append(ul);
 }
 
