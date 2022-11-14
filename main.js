135b793d5c7287ee5878e59c9e6921bf892d473a
jesnagbg <j.m.pettersson@hotmail.se>
2022-11-14 23:35:41 +0100

    buttons work

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const itemList = ["Broom", "Potatoes"];
+let itemList = ["Broom", "Potatoes"];
+const choseWeapon = ["Sword", "Broom", "Needle"];
 const nameInput = document.getElementById("name-input");
 const nameButton = document.getElementById("start-button");
 //const spareQuestion = document.getElementById("extra-question");
@@ -7,19 +8,6 @@ let playerName = "Farmer";
 window.addEventListener("DOMContentLoaded", displayItems);
 
 nameButton.addEventListener("click", savePlayerName);
-
-
-function savePlayerName() {
-    playerName = document.getElementById("name-input").value;
-    console.log(playerName);
-    
-    oneMoreQ()
-}
-
-function oneMoreQ() {
-    const extraQuestion = document.getElementById("extra-question");
-    extraQuestion.textContent = 'Oh, right. Sorry, ' + playerName + '. Just one more thing! Chose your weapon';
-}
 
 function displayItems() {
     const div = document.getElementById("item-container");
@@ -35,4 +23,42 @@ function displayItems() {
     div.innerHTML = "";
     div.append(ul);
 }
+
+function savePlayerName() {
+    playerName = document.getElementById("name-input").value;
+    console.log(playerName);
+    
+    oneMoreQ();
+}
+
+function oneMoreQ() {
+    const extraQuestion = document.getElementById("extra-question");
+    extraQuestion.textContent = 'Oh, right. Sorry, ' + playerName + '. Just one more thing! Chose your weapon';
+
+    choseYourWeapon();
+}
+
+function choseYourWeapon() {
+    clear();
+    const div = document.getElementById("button-container");
+    
+    for (const weapon of choseWeapon) {
+        const button = document.createElement("button");
+        button.textContent = weapon;
+        button.addEventListener('click', function() {
+            itemList.push(weapon);
+            displayItems();
+        })
+        div.append(button);
+    }    
+}
+
+function clear() {
+    document.getElementById("button-container").innerHTML = "";
+}
+
+
+
+
+
 
